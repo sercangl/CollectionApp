b5e400da679deae71a047dd4c5f9b2cf970a0f5b
Sercan Gül <33196225+sercangl@users.noreply.github.com>
2018-12-23 16:00:20 +0000

    Add files via upload

---
+++ Client/src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {}
---
+++ Client/src/sample/NewD.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NewD extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("NewD.fxml"));
+        primaryStage.setTitle("Collection System");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @FXML
+    private void preesCd(ActionEvent e) throws Exception {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("NewD.fxml"));
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+    }
+}
